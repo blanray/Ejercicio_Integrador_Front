d15ce8b61c78611c9888728da85f9f0fe947bbe7
blanray <blanray_winner@hotmail.com>
2022-05-08 19:10:08 -0300

    "Version con tarjetas 1"

--- js/misFunciones.js
+++ js/misFunciones.js
@@ -1,6 +1,8 @@
 function ocultarCuerpo() {
 
     document.getElementById("cuerpoPrincipal").classList.toggle("ocultarSeccion");
+    document.getElementById("formularioCompra").classList.toggle("ocultarSeccion");
+
 }
 
 document.getElementById("comprarTicket").onclick = function() {
