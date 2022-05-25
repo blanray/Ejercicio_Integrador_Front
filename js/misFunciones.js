df11f1a4c972f86def97f0462dbf745afbde0dd3
blanray <blanray_winner@hotmail.com>
2022-05-24 21:04:53 -0300

    Version con Event Listener

--- js/misFunciones.js
+++ js/misFunciones.js
@@ -1,16 +1,12 @@
+var comprarTicket = document.getElementById("comprar-ticket")
+comprarTicket.addEventListener('click', ocultarCuerpo)
+
+var comprarTicket2 = document.getElementById("comprar-ticket2")
+comprarTicket2.addEventListener('click', ocultarCuerpo)
+
 function ocultarCuerpo() {
 
     document.getElementById("cuerpoPrincipal").classList.toggle("ocultarSeccion");
     document.getElementById("formularioCompra").classList.toggle("ocultarSeccion");
 
-}
-
-document.getElementById("comprarTicket").onclick = function() {
-
-    ocultarCuerpo();
-}
-
-document.getElementById("comprarTicket2").onclick = function() {
-
-    ocultarCuerpo();
 }
